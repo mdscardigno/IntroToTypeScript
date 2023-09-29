8ac28c8ad8854b559974e7c4323d6348493c2d9b
Milagro Scardigno <mdscardigno@gmail.com>
2023-09-28 20:13:49 -0400

    Variables in Typescript

--- src/example.js
+++ src/example.js
@@ -49,7 +49,118 @@ const teachers = ['Mr. Smith', 'Mrs. Jones']
 let scores = [98, 100, 78, 99, 100]
 //javascript can mix and match
 const differentThings = ['Mary', 98, true, null, undefined, { name: 'Mary' }]
-const differentKindsOfThings = [42, 'Ice Cream', true, 100, 'Pizza']
+const differentKindsOfThings = [42, 'Ice Cream', 100, 'Pizza']
+//
+//:string
+//
+//^^^ this is Typescript specific. This is NOT valid JS
+//Since this is me teaching Typescript.
+// const differentMoreKindsOfThings: (string | number | boolean)[] = [
+//   42,
+//   'Ice Cream',
+//   100,
+//   'Pizza',
+//   98,
+// ]
 //the pipe symbol is an OR
 //explicitly typed array
 // let studentName: string = 'Mary' this can only be done in ts files
+//
+//The inference: let TypeScript figure out the type until it cant.
+//
+differentKindsOfThings.push('Candy')
+differentKindsOfThings.push(98)
+// differentKindsOfThings.push(true) it knows I am trying to push a boolean so we add | boolean to the types
+// so it is better to be explicit with the type
+//so we will end up with: (string | number | boolean)[]
+//const anyKindsOfThings: any[] = ['Mary', 98, true, null, undefined, { name: 'Mary' }]
+//
+//const personName = 'Mila'
+//personName = 'Mary' // error. So why is the Push working? Look at the notes bellow about what is a const
+//
+//const grade = 98
+//grade = grade + 1 I am computing a new value for grade and reassigning it. So
+//This is not changing the value of grade, this is reassigning the value of grade
+//const doesnt mean that the value of the const cannot change,
+//it means that the variable cannot be reassigned the variable to point to a different value
+//but what const really means is that variable name cant appear on the left side of an assignment operator again
+//const =  unreassignable variable
+//strings and numbers are inmutable so you cannot change them
+//unlike an array that can grow and shrink therefore are mutable
+//booleans are inmutable: true cant be changed to false and viceversa
+//NOTE: I cannot completely reasign an array, but
+//I can change the contents of the array by using push, pop, shift, unshift, splice, etc
+//
+//to be continued on 1:31:00 *UNDEFINED VARIABLES*
+
+// let studentName: any
+// studentName = 'Mary'
+// studentName = 42
+// studentName = true
+// studentName = [1, 2, 3]
+// studentName = { name: 'Mary' }
+// studentName = null
+// studentName = undefined
+//any is the default type for variables in javascript
+//
+//Typescript specific:
+// let studentName: string
+//What is the value of studentName on this line
+//--------?????----------
+//studentName is a valueless variable. It is undefined.
+//null vs undefined
+//
+//types vs values
+//types are the kinds of things that we can have in our program
+//values are the actual things that we have in our program
+//null and undefined are types
+//null and undefined are values
+//we need to start thinking about the classification of things more carefully
+//null means, i assigned something to it and with that i mean the absence of any value
+//undefined means the absence of anything whatsoever
+//
+//so in the example of let studentName: string
+//studentName is undefined because it has not been assigned a value
+//we can also say something like let studentName: string | undefined
+//so how can it be undefined if we are saying it is a string?
+//because we are saying that it can be undefined or a string so it is undefined and this is where
+//js and ts break down a little bit
+//union types are a ts concept
+//https://www.w3schools.com/typescript/typescript_union_types.php
+//so Typescript is saying that studentName is undefined until you give it a value
+//****THIS BELLOW IS ILLEGAL***
+//so studentName: string
+//studentName = 'Mary'
+//studentName = undefined
+//***THIS BELOW IS LEGAL***
+//let studentName: string | undefined
+//studentName = 'Mary'
+//studentName = undefined
+//
+//It is preferred to do:
+//let studentName = ""
+//studentName = 'Mary'
+//to be continued on 1:40:00 *NULL*
+//avoid undefined and any
+//Bad Form:
+//let name --> undefined and is of type any
+//name = 'Jane' --> name is now contains the value 'Jane', but `name` is still `any` type.
+//Better Form:
+//let name: string --> undefined and is of type string
+//name = 'Jane'   --> name is now contains the value 'Jane'.
+//Best Form:
+//const name = 'Jane' --> name is now contains the value 'Jane' and is of type string. We avoid  any issues with undefined
+//best way is to defined variable as let or const but if you are not going to reassign the variable then use const.
+//Having immutable data types makes stronger code and less error prone. Allows the compiler and the computer to make more assumptions about our code
+//The more we can avoid changing the value of a variable the better and the lower the chances there are for bugs
+//Good variable hygiene
+//Follow these rules and you will do well:
+//Only use const and let
+//Use const unless you have a good reason to use let
+//Always initialize a variable unless you have a good reason not to
+//
+//Coversions: Typescript is more forgiving when converting types
+//Valid in Typescript but not allowed in a language like C#
+//const X - 'The answer is ' + 42 // "The answer is 42"
+//const Y = 42 + ' is the answer' // "42 is the answer"
+//check ts file for snippets of code
--- src/main.ts
+++ src/main.ts
@@ -4,23 +4,23 @@ import './style.css'
 //
 // ^^^^ this is Typescript specific, it is not valid JS
 //      since this is the type of the variable
-let students = ['Mary', 'John', 'Jane']
-const teachers = ['Mr. Smith', 'Mrs. Jones']
-let scores = [98, 100, 78, 99, 100]
-const personName = 'Mary' //strings are immutable
+//let students = ['Mary', 'John', 'Jane']
+//const teachers = ['Mr. Smith', 'Mrs. Jones']
+//let scores = [98, 100, 78, 99, 100]
+//const personName = 'Mary' //strings are immutable
 // personName = 'Gavin' <---- this is an error because personName is a const but why in the array bellow
 // I am pushing something therefore changing the array?
 //const doesnt mean that the value cant change, but it means you cant reassign the variable to point to something else
 //const is a pointer to a value, not the value itself
 //for example
-const grade = 98
-grade = grade + 1 //this is an error because you are trying to reassign the variable to point to something else
+//const grade = 98
+//grade = grade + 1 //this is an error because you are trying to reassign the variable to point to something else
 //it is not changing the value of grade but reassigning the variable to point to something else
 //therefore what cons means is that the variable name cannot appear on the left side of an assignment operator or equal sign
 //javascript can mix and match
 // const differentKindsOfThings = [] would take an array of anything because it doesnt know
 //you can put any[] to make it an array of anything but it is not recommended
-const differentThings: (
+/*const differentThings: (
   | string
   | number
   | boolean
@@ -29,14 +29,25 @@ const differentThings: (
   | undefined
 )[] = ['Mary', 98, true, null, undefined, { name: 'Mary' }]
 differentThings.push('3.14159265')
-differentThings.push('Tim') //arrays are mutable and can be changed even if they are const
+differentThings.push('Tim')*/ //arrays are mutable and can be changed even if they are const
 //remember that once you push a string into an array of string you will get a different array of strings
-const differentKindsOfThings = [42, 'Ice Cream', true, 100, 'Pizza']
+//const differentKindsOfThings = [42, 'Ice Cream', true, 100, 'Pizza']
 //the pipe symbol is an OR
 
 //explicitly typed array
-let studentName: string = 'Mary'
+/*let studentName: string = 'Mary'
 let studentNames: string[] = ['Mary', 'John', 'Jane']
-let studentScores: number[] = [98, 100, 78, 99, 100]
+let studentScores: number[] = [98, 100, 78, 99, 100]*/
 //to be continued on 1:31:15
 //Undefined Variables
+//RunJS tool
+const x = 'The answer is ' + 42
+const y = 42 + ' is The answer'
+
+let score = 100
+score
+score = score + 5
+//Console.WriteLine(score)
+console.log('Score is ', score)
+console.log('x is ', x)
+console.log('y is ', y)
